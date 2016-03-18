fa44c8e507acb5e239a1ec8d7cf021ac594b60ce
Samvel Avanesov <seavan@gmail.com>
2016-03-18 17:39:29 +0200

    updating data collection opt-in

--- src/js/jsx/signup/signup_wizard_optin.jsx
+++ src/js/jsx/signup/signup_wizard_optin.jsx
@@ -21,11 +21,9 @@
         render: function () {
             return (
                 <div className="animate-enter">
-                    <div className="headline">Would you like to help with usability research?</div>
-                    <p>By enabling anonymous data collection, we will collect 
-                        non-identifying and non-content information to share with researchers and improve Peerio. <Peerio.UI.Tappable onTap={Peerio.NativeAPI.openInBrowser.bind(this, 'https://peerio.zendesk.com/hc/en-us/articles/203946145')}><b><u>Tap here</u></b> to learn which data we collect.</Peerio.UI.Tappable></p>
-                    <p>We understand your data has value. When you opt in, 
-                        we will add 25MB to your account everyday as thanks for your contribution.</p>
+                    <div className="headline">Want to aid usability research?</div>
+                    <p>You can opt-in for anonymous data collection. This allows Peerio to collect non-identifying and non-content information to share with researchers and improve Peerio.<Peerio.UI.Tappable onTap={Peerio.NativeAPI.openInBrowser.bind(this, 'https://peerio.zendesk.com/hc/en-us/articles/203946145')}><b><u>Tap here</u></b> to learn which data we collect.</Peerio.UI.Tappable></p>
+                    <p>If you opt-in, we will add 25MB to your account everyday as thanks for your contribution.</p>
                     <div className="buttons">
                         <Peerio.UI.Tappable 
                             element="div" 
