3863e034e65c29df1eaa14f71a9cc0b7407046ba
Samvel Avanesov <seavan@gmail.com>
2016-03-18 17:46:33 +0200

    adding space

--- src/js/jsx/signup/signup_wizard_optin.jsx
+++ src/js/jsx/signup/signup_wizard_optin.jsx
@@ -22,7 +22,9 @@
             return (
                 <div className="animate-enter">
                     <div className="headline">Want to aid usability research?</div>
-                    <p>You can opt-in for anonymous data collection. This allows Peerio to collect non-identifying and non-content information to share with researchers and improve Peerio.<Peerio.UI.Tappable onTap={Peerio.NativeAPI.openInBrowser.bind(this, 'https://peerio.zendesk.com/hc/en-us/articles/203946145')}><b><u>Tap here</u></b> to learn which data we collect.</Peerio.UI.Tappable></p>
+                    <p>You can opt-in for anonymous data collection. This allows Peerio to collect non-identifying and non-content information to share with researchers and improve Peerio.
+                    </p> 
+                    <p><Peerio.UI.Tappable onTap={Peerio.NativeAPI.openInBrowser.bind(this, 'https://peerio.zendesk.com/hc/en-us/articles/203946145')}><b><u>Tap here</u></b> to learn which data we collect.</Peerio.UI.Tappable></p>
                     <p>If you opt-in, we will add 25MB to your account everyday as thanks for your contribution.</p>
                     <div className="buttons">
                         <Peerio.UI.Tappable 
