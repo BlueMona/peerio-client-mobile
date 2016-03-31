84064ba207d671f6a18af1b2ceb9e041f40c5d23
Paul Dukes <paul.r.dukes@gmail.com>
2016-03-31 15:41:21 -0400

    updates to styling

--- src/js/jsx/signup/signup_wizard_tos.jsx
+++ src/js/jsx/signup/signup_wizard_tos.jsx
@@ -8,14 +8,14 @@
       },
 
       handleTOS: function () {
-          console.log('TOS!');
           Peerio.NativeAPI.openInBrowser('https://github.com/PeerioTechnologies/peerio-documentation/blob/master/Terms_of_Use.md')
       },
 
       render: function () {
           return (
               <div style={{marginTop: '160px'}}>
-                  <p>By signing up for a Peerio account, you agree to our <Peerio.UI.Tappable element="a" style={{textDecoration: 'underline', color: '#fff'}} onTap={this.handleTOS}>terms of use</Peerio.UI.Tappable>.</p>
+                <div className="headline">Terms of Use</div>
+                  <p>Before signing up for a Peerio account, we ask that you agree to our <Peerio.UI.Tappable element="a" style={{textDecoration: 'underline', color: '#fff'}} onTap={this.handleTOS}>terms of use</Peerio.UI.Tappable>.</p>
                   <div className="buttons">
                        <Peerio.UI.Tappable
                           element='div'
