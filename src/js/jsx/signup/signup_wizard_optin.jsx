5425c23676c436d09db888ebf4e069be90cb7319
Paul Dukes <paul.r.dukes@gmail.com>
2016-04-01 09:31:55 -0400

    moved button for consistency.

--- src/js/jsx/signup/signup_wizard_optin.jsx
+++ src/js/jsx/signup/signup_wizard_optin.jsx
@@ -23,18 +23,19 @@
                 <div className="animate-enter">
                     <div className="headline">Want to aid usability research?</div>
                     <p>You can opt-in for anonymous data collection. This allows Peerio to collect non-identifying and non-content information to share with researchers and improve Peerio.
-                    </p> 
+                    </p>
                     <p><Peerio.UI.Tappable onTap={Peerio.NativeAPI.openInBrowser.bind(this, 'https://peerio.zendesk.com/hc/en-us/articles/203946145')}><b><u>Tap here</u></b> to learn which data we collect.</Peerio.UI.Tappable></p>
                     <p>If you opt-in, we will add 25MB to your account everyday as thanks for your contribution.</p>
                     <div className="buttons">
-                        <Peerio.UI.Tappable 
-                            element="div" 
-                            className="btn-primary" 
-                            onTap={this.handleDataOptIn.bind(this, false)}>Not right now</Peerio.UI.Tappable>
-                        <Peerio.UI.Tappable 
-                            element="div" 
-                            className="btn-safe" 
+                        <Peerio.UI.Tappable
+                            element="div"
+                            className="btn-safe"
                             onTap={this.handleDataOptIn.bind(this, true)}>Yes, I do</Peerio.UI.Tappable>
+                        <Peerio.UI.Tappable
+                            element="div"
+                            className="btn-primary"
+                            onTap={this.handleDataOptIn.bind(this, false)}>Not right now</Peerio.UI.Tappable>
+
                     </div>
                 </div>);
         },
