24e286d20999e7ceb9a50de4257c22739af73c88
Samvel Avanesov <seavan@gmail.com>
2016-05-04 02:28:18 +0300

    adding build for testing in-app purchases with debugging

---
+++ release_debuggable.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# stop execution on error
+set -e
+
+echo ==================== COMPILING ASSETS ====================
+gulp compile --release
+# gulp compile
+
+
+echo =============== BUILDING ANDROID PROJECT =================
+python tools/add-manifest-debuggable.py
+cordova build --release android
+
+function signapk(){
+  echo ==========================================================
+  echo "SIGNING: $1 => $2"
+  echo ==========================================================
+  jarsigner -verbose -sigalg SHA256withRSA -digestalg SHA-256 -keystore peerio.keystore $1 peerio_release_key
+
+  echo ========== VERIFYING ==========
+  jarsigner -verify -verbose -certs $1
+
+  echo ========== ZIPALIGN ==========
+  zipalign -f -v 4 $1 $2
+}
+mkdir -p ./bin
+signapk ./platforms/android/build/outputs/apk/android-x86-release-unsigned.apk ./bin/peerio-x86.apk
+
+echo ============================================
+echo "||             BUILD SUCCESS              ||"
+echo ============================================
+
+adb uninstall com.peerio
+adb install ./bin/peerio-x86.apk
+adb shell am start -n com.peerio/com.peerio.MainActivity
+
