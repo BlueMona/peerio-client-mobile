ee1a34554245721875156fafdbb09c67089c5816
Samvel Avanesov <seavan@gmail.com>
2016-05-11 15:15:07 +0300

    removing redeem coupon for iOS

--- src/js/jsx/layout_items/sidebar.jsx
+++ src/js/jsx/layout_items/sidebar.jsx
@@ -111,10 +111,11 @@
                                         <i className="material-icons">settings</i> {t('preferences')}
                                     </Peerio.UI.Tappable>
 
+                                    {Peerio.runtime.platform != 'ios' ?
                                     <Peerio.UI.Tappable tag="li"
                                                         onTap={this.toggleAndTransition.bind(this, 'enter_coupon')}>
                                         <i className="fa fa-certificate"></i> {t('redeemCoupon')}
-                                    </Peerio.UI.Tappable>
+                                    </Peerio.UI.Tappable> : null}
                                 </ul>
                                 <ul>
                                     <Peerio.UI.Tappable element="li" onTap={this.handleSupport}>
