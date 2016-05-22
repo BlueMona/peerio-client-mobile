ef6b017f1fd7be11b6b281b06e7081f02bc3c128
Samvel Avanesov <seavan@gmail.com>
2016-05-22 18:41:22 +0300

    adding install tracking to iOS

---
+++ src/js/google_conversion.js
@@ -0,0 +1,39 @@
+/**
+ * Google AdWords conversion helper
+ * ------------------------------------------------------
+ */
+var Peerio = this.Peerio || {};
+Peerio.GoogleConversion = Peerio.GoogleConversion || {};
+
+Peerio.GoogleConversion.init = function () {
+    'use strict';
+
+    var api = Peerio.GoogleConversion;
+
+    api.getIDFA = function () {
+        if(Peerio.runtime.platform != 'ios') {
+            L.info('No Apple advertising plugin for this platform. Returning mock');
+            return Promise.resolve('8880ea97-db86-4e10-a10f-4e71461ab8f9');
+        }
+        if(!window.plugins.AppleAdvertising) throw 'Plugin AppleAdvertising not found';
+        return new Promise( (resolve, reject) => {
+            window.plugins.AppleAdvertising.getIDFA(resolve, reject);
+        });
+    };
+
+    api.testIDFA = function () {
+        api.getIDFA()
+        .then(i => Peerio.UI.Alert.show({ text: i, serviceClass: '_googleConversion' }));
+    };
+
+    api.trackInstall = function () {
+        api.getIDFA()
+        .then(i => { 
+            L.info('Mocking track install ' + i);
+            api.idfa = i;
+        });
+    };
+
+    Peerio.runtime.platform == 'ios' && Peerio.runtime.firstRun && api.trackInstall();
+
+};
