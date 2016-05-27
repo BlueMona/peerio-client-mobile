cd250da3bcabacc4d5f6379359086b0984ef00f5
Samvel Avanesov <seavan@gmail.com>
2016-05-27 18:00:56 +0300

    improving restore capability

--- tools/iphone-restore.sh
+++ tools/iphone-restore.sh
@@ -6,8 +6,6 @@ idevicediagnostics restart
 sleep 55
 # wait until the iPhone is fully booted
 python tools/iphone-wait-restore.py
-# restart to make the nasty iOS alerts go away (they interfere with testing)
-# idevicediagnostics restart
-# python tools/iphone-wait-reboot.py
-# sleep 5
+cordova run ios
+sleep 15
 echo "Device ready"
