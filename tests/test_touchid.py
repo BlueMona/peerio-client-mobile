a1d00d7e3da2be6b4558015a820edaf9a9901c61
Samvel Avanesov <seavan@gmail.com>
2016-06-04 21:50:21 +0300

    adding touchid test capability

--- tests/test_touchid.py
+++ tests/test_touchid.py
@@ -1,12 +1,18 @@
 import common.testcase
+import time
 from common.helper import *
 
 class TouchID(common.testcase.TestCase):
     def test_01_push(self):
         if driver().wipe:
             driver().wipe()
+        if driver().enable_touchid:
+            driver().enable_touchid()
+            time.sleep(5)
         hasFeature = execute_script("return Peerio.UI.TouchId.test()")
         if hasFeature == 'unsupported':
             return
-        idfa = get_text_by_css('._touchID p')
-        assert len(idfa) > 0
+        res = get_text_by_css('._touchID p')
+        assert len(res) > 0
+        if driver().enable_touchid:
+            assert res.find('true') != -1
