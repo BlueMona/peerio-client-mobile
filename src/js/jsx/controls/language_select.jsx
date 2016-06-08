7f75b4592789097c1fd042ccf286c814065d65d8
Samvel Avanesov <seavan@gmail.com>
2016-06-08 17:39:38 +0300

    dropdown for arrow

--- src/js/jsx/controls/language_select.jsx
+++ src/js/jsx/controls/language_select.jsx
@@ -5,27 +5,39 @@
             var locale = event.target.value;
             Peerio.Translator.loadLocale(locale);
             Peerio.Helpers.savePreferredLocale(locale);
-    
+
             if(Peerio.user)
                 Peerio.user.setLocale(locale);
             else
                 Peerio.Translator.forceLocaleOnLogin = true;
         },
+
+        showDropdown: function () {
+            var event = document.createEvent('MouseEvents');
+            event.initMouseEvent('mousedown', true, true, window);
+            this.refs.select.getDOMNode().dispatchEvent(event);
+        },
+
         // TODO refactor this to a generic select
         render: function () {
             return (
                 <div className="input-select">
-                      <select id="language-select"
-                          onChange={this.changeLocale}>
-                          {
-                              Peerio.Config.locales.map(l => l.code === Peerio.Translator.locale ?
-                              <option value={l.code} selected>{l.name}</option> :
-                              <option value={l.code} >{l.name}</option>)
-                          }
-                      </select>
-                      <i className="material-icons">arrow_drop_down</i>
-              </div>
-          );
+                    <select ref="select"
+                            id="language-select"
+                            onChange={this.changeLocale}>
+                        {
+                            Peerio.Config.locales.map(l => l.code === Peerio.Translator.locale ?
+                        <option value={l.code} selected>{l.name}</option> :
+                        <option value={l.code} >{l.name}</option>)
+                        }
+                    </select>
+                    <Peerio.UI.Tappable tag="i"
+                                        className="material-icons"
+                                        onTap={this.showDropdown}>
+                        arrow_drop_down
+                    </Peerio.UI.Tappable>
+                </div>
+            );
         }
     });
 }());
