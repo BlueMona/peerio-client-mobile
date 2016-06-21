5ac0d35c7224f31b7518c722b0f73f4c76447b31
Samvel Avanesov <seavan@gmail.com>
2016-06-21 13:41:23 +0300

    making date locale respect user locale settings instead of phone locale settings

--- src/js/jsx/payments/payments_subscription_list_item.jsx
+++ src/js/jsx/payments/payments_subscription_list_item.jsx
@@ -10,7 +10,7 @@
                     <div className="info-content">  {s.bonus} - {s.plan_name}</div>
 
                     <label>{t('payments_endDate')}</label>
-                    <div className="info-content">{new Date(s.current_period_end).toLocaleDateString()}</div>
+                    <div className="info-content">{new Date(s.current_period_end).toLocaleDateString(Peerio.Translator.locale)}</div>
 
                     {/*<li>{t('payments_quota')}: {s.quota/1024/1024/1024}</li>*/}
                     {/*<li>{t('payments_status')}: {s.status}</li>*/}
