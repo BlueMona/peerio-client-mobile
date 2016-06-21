54867b29137b4c9da8e2a1e4af51dfeba22ce07f
Paul Dukes <paul.r.dukes@gmail.com>
2016-06-21 11:49:05 -0400

    ghost share screen. We’re dropping the copy options from the share page. I’m updating InVision to reflect this change.

---
+++ src/js/jsx/ghosts/ghost_share.jsx
@@ -0,0 +1,37 @@
+(function () {
+    'use strict';
+
+    Peerio.UI.ShareGhost = React.createClass({
+
+        render: function () {
+            return (
+                <div className="content without-tab-bar">
+                      <div className="headline">{t('ghost_share')}</div>
+                      <p>{t('ghost_sent')} + ghostRecipient</p>
+                      <p>{t('ghost_passphrase_share')}</p>
+
+                      <label>{t(passphrase)}</label>
+                      {/* generated passphrase goes */}
+
+                      <Peerio.UI.Tappable element="i" onTap={this.shareEverything}
+                                          className="material-icons">
+                          share
+                      </Peerio.UI.Tappable>
+
+                      <div className="caption">
+                            {t('ghost_passphrase_share_helper')}
+                      </div>
+                      <p>{t('ghost_passphrase_share_link')}</p>
+
+                      <div className="flex-row">
+                          {/* link */}
+                          <Peerio.UI.Tappable element="i" onTap={this.shareEverything}
+                                              className="material-icons">
+                              share
+                          </Peerio.UI.Tappable>
+                      </div>
+                </div>
+            );
+        }
+    });
+} ());
