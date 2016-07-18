431b5a6ef71b4de5ce904faff6516b80dd83611c
Samvel Avanesov <seavan@gmail.com>
2016-07-18 16:36:18 +0300

    updating subscriptions page to match new server format

--- src/js/jsx/payments/payments_subscription_list_item.jsx
+++ src/js/jsx/payments/payments_subscription_list_item.jsx
@@ -7,7 +7,18 @@
             return (
                   <li className="list-item">
                     <label>{t('payments_planName')}</label>
-                    <div className="info-content">  {s.bonus} - {s.plan_name}</div>
+                    <div className="info-content">{s.plan_name}</div>
+                      {s.bonus.file ? 
+                          <div>
+                            <label>{t('payments_extraStorage')}</label>
+                            <div className="info-content">{s.bonus.file.limit/1024/1024/1024}GB</div>
+                          </div> : null}
+
+                      {s.bonus.ghost ? 
+                          <div>
+                            <label>{t('payments_ghostLimitMonthly')}</label>
+                            <div className="info-content">{s.bonus.ghost.limit === null ? t('payments_unlimited') : s.bonus.ghost.limit}</div>
+                          </div> : null}
 
                     <label>{t('payments_endDate')}</label>
                     <div className="info-content">{new Date(s.current_period_end).toLocaleDateString(Peerio.Translator.locale)}</div>
