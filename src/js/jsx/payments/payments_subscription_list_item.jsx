b52782e7715518e1dfffc9006ce9aaacff4facbc
paulrdukes <paul.r.dukes@gmail.com>
2016-07-18 12:02:46 -0400

    subscriptions page updates

--- src/js/jsx/payments/payments_subscription_list_item.jsx
+++ src/js/jsx/payments/payments_subscription_list_item.jsx
@@ -5,27 +5,29 @@
         render: function () {
             var s = this.props.subscription;
             return (
-                  <li className="list-item">
-                    <label>{t('payments_planName')}</label>
-                    <div className="info-content">{s.plan_name}</div>
-                      {s.bonus.file ? 
-                          <div>
-                            <label>{t('payments_extraStorage')}</label>
-                            <div className="info-content">{s.bonus.file.limit/1024/1024/1024}GB</div>
-                          </div> : null}
+                    <ul className="flex-list">
+                        <li className="list-item">
+                            <label>{t('payments_planName')}</label>
+                            <div className="info-content">{s.plan_name}</div>
+                        </li>
+                        {s.bonus.file ?
+                            <li className="list-item">
+                                <label>{t('payments_extraStorage')}</label>
+                                <div className="info-content">{s.bonus.file.limit/1024/1024/1024}GB</div>
+                            </li> : null}
 
-                      {s.bonus.ghost ? 
-                          <div>
-                            <label>{t('payments_ghostLimitMonthly')}</label>
-                            <div className="info-content">{s.bonus.ghost.limit === null ? t('payments_unlimited') : s.bonus.ghost.limit}</div>
-                          </div> : null}
-
-                    <label>{t('payments_endDate')}</label>
-                    <div className="info-content">{new Date(s.current_period_end).toLocaleDateString(Peerio.Translator.locale)}</div>
-
-                    {/*<li>{t('payments_quota')}: {s.quota/1024/1024/1024}</li>*/}
-                    {/*<li>{t('payments_status')}: {s.status}</li>*/}
-                  </li>
+                          {s.bonus.ghost ?
+                              <li className="list-item">
+                                  <label>{t('payments_ghostLimitMonthly')}</label>
+                                  <div className="info-content">{s.bonus.ghost.limit === null ? t('payments_unlimited') : s.bonus.ghost.limit}</div>
+                              </li> : null}
+                        <li className="list-item">
+                            <label>{t('payments_endDate')}</label>
+                            <div className="info-content">{new Date(s.current_period_end).toLocaleDateString(Peerio.Translator.locale)}</div>
+                        </li>
+                        {/*<li>{t('payments_quota')}: {s.quota/1024/1024/1024}</li>*/}
+                        {/*<li>{t('payments_status')}: {s.status}</li>*/}
+                    </ul>
             );
         }
     });
