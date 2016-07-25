ef1148a94ff099692ac31519e8ca2fbca6730a92
Samvel Avanesov <seavan@gmail.com>
2016-07-26 01:26:15 +0300

    making important modals STACK

--- src/js/jsx/modal/modal_manager.jsx
+++ src/js/jsx/modal/modal_manager.jsx
@@ -32,10 +32,10 @@
             this.subscriptions = [
                 Peerio.Dispatcher.onShowAlert(this.showModal.bind(this, Peerio.UI.Alert)),
                 Peerio.Dispatcher.onShowConfirm(this.showModal.bind(this, Peerio.UI.Confirm)),
-                Peerio.Dispatcher.onShowPrompt(this.showModal.bind(this, Peerio.UI.Prompt)),
-                Peerio.Dispatcher.onShowContactSelect(this.showModal.bind(this, Peerio.UI.ContactSelect)),
-                Peerio.Dispatcher.onShowFileSelect(this.showModal.bind(this, Peerio.UI.FileSelect)),
-                Peerio.Dispatcher.onShowFileUpload(this.showModal.bind(this, Peerio.UI.Upload)),
+                Peerio.Dispatcher.onShowPrompt(this.showModal.bind(this, Peerio.UI.Prompt, {stacked: true})),
+                Peerio.Dispatcher.onShowContactSelect(this.showModal.bind(this, Peerio.UI.ContactSelect, {stacked: true})),
+                Peerio.Dispatcher.onShowFileSelect(this.showModal.bind(this, Peerio.UI.FileSelect, {stacked: true})),
+                Peerio.Dispatcher.onShowFileUpload(this.showModal.bind(this, Peerio.UI.Upload, {stacked: true})),
                 Peerio.Dispatcher.onSyncStarted(this.showModal.bind(this, Peerio.UI.Sync, {stacked: true})),
 
                 Peerio.Dispatcher.onRemoveModal(this.removeModal)
