682ce1f0adf2ad9d215c5cbf2090b77c0781c411
Samvel Avanesov <seavan@gmail.com>
2016-07-25 13:43:02 +0300

    adding onboarding text and greeting

--- src/js/jsx/app.jsx
+++ src/js/jsx/app.jsx
@@ -9,15 +9,19 @@
                 Peerio.Dispatcher.onTransitionTo(this.handleTransition),
                 Peerio.Dispatcher.onResume(this.handleResume),
                 Peerio.Dispatcher.onAuthenticated(this.checkTOS),
-                Peerio.Dispatcher.onSettingsUpdated(this.checkTOS)
+                Peerio.Dispatcher.onSettingsUpdated(this.checkTOS),
+                Peerio.Dispatcher.onSettingsUpdated(this.checkClientVersion)
             ];
         },
+
         componentDidMount: function () {
             this.checkTOS();
         },
+
         componentWillUnmount: function () {
             Peerio.Dispatcher.unsubscribe(this.subscriptions);
         },
+
         checkTOS: function () {
             setTimeout(()=> {
                 if (this.tosRequested || Peerio.user.settings.acceptedLatestTOS) return;
@@ -41,20 +45,45 @@
                     .finally(()=> this.tosRequested = false);
             }, 2000);
         },
+
+        checkClientVersion: function () {
+            var version = Peerio.user.latestClientVersions && Peerio.user.latestClientVersions.mobile;
+            version = version || '0.0.0';
+            var currentVersion = Peerio.runtime.version;
+            if(!semver.valid(version) || !semver.valid(currentVersion)) {
+                L.error('Invalid user client version provided: ' + version + ' ' + currentVersion);
+                return;
+            }
+
+            var GHOST_MILESTONE = '2.5.7'; 
+
+            if(semver.compare(currentVersion, version) > 0) {
+                // we got the newer version run for the first time
+                // time for some action!
+                if(semver.compare(currentVersion, GHOST_MILESTONE) >= 0)
+                    Peerio.UI.Alert.show({text: t('ghostOnboardText'), headline: t('ghostOnboardTitle')});
+                Peerio.user.updateMobileClientVersion(currentVersion);
+            }
+        },
+
         handleTOSRead: function () {
             Peerio.NativeAPI.openInBrowser('https://github.com/PeerioTechnologies/peerio-documentation/blob/master/Terms_of_Use.md');
         },
+
         handleHardwareBack: function () {
             if (this.isAppRoot()) return;
             this.goBack();
         },
+
         handleResume: function () {
             Peerio.NativeAPI.clearPushBadge();
         },
+
         // hack to allow out of router context components to navigate
         handleTransition: function () {
             this.transitionTo.apply(this, arguments);
         },
+
         render: function () {
             return (
                 <div>
