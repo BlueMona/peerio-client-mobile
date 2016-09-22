04960e835147507446ccf292977cb43893a7cc31
paulrdukes <paul.r.dukes@gmail.com>
2016-09-21 22:46:02 -0400

    basic information is now profile

--- src/js/jsx/signup/signup_wizard_name.jsx
+++ src/js/jsx/signup/signup_wizard_name.jsx
@@ -71,7 +71,7 @@
         render: function () {
             return (
                 <fieldset key={'signup-step-0'} className="animate-enter">
-                    <div className="headline">{t('basicInformation')}</div>
+                    <div className="headline">{t('profile')}</div>
                     <Peerio.UI.TrackSubState name="basic"/>
 
                     <div className="input-group">{
